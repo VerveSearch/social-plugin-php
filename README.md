9e78f4241c74b878e99ce091ea3380d080f1ab07
anastasiatim <anastaiatim@yandex.ru>
2015-08-07 10:34:11 +0100

    Version 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # VerveSearch Socializer
 
+A PHP Class that generates the  URLs for Facebook, LinkedIn, Twitter and GPlus.
+
+## Usage example
+
+$obj = new Socializer();
+$twitterUrl = $obj->generateFacebookUrl('http://www.vervesearch.com/');
--- src/Socializer.php
+++ src/Socializer.php
@@ -3,11 +3,14 @@
 namespace VerveSearch\Plugins; 
 
 /**
- * 
+ *  Plugin to generate Social Sharing links;
  * 
  * 
  */
+ 
+
 class Socializer {
+
 	public function __construct(){
 		
 	}
@@ -18,19 +21,79 @@ class Socializer {
 	 * @param string $text the text to add to the share story (max 150 characters)
 	 * @return string the Facebook share URL. 
 	 */
-	public function generateFacebookUrl($url,$text){
 
+	public function generateFacebookUrl($url,$title=null,$summary=null,$image=null){
+		$shareUrl  = 'https://www.facebook.com/sharer/sharer.php?s=100&p[url]='.$url;
+		if($image){
+			$shareUrl = '&p[images][0]='.$image;
+		}
+		if($title){
+			$shareUrl = '&p[title]='.$title;
+		}
+		if($summary){
+			$shareUrl = '&p[summary]='.$summary;
+		}
+
+		return $shareUrl; 	 
 	}
+
+	/**
+	 * Generates twitter share URL given the url and text, via and hashrags are optional
+	 * @param string $url the url to generate the share link for twetter
+	 * @param string $text the text to tweet (max 150 characters)
+	 * @param string $via the channel name. No '@'
+	 * @param string $hashtags the list of comma-separeted hashtags, no '#'
+	 * @return string the Twitter share URL. 
+	 */
 
 	public function generateTwitterUrl($url,$text,$via=null,$hashtags=null){
-
+		$shareUrl  ='https://twitter.com/intent/tweet?text=';
+		$shareUrl .= urlencode($text);
+		if($hashtags){
+			$shareUrl .= '&hashtags='.$hashtags;
+		}
+		if($via){
+			$shareUrl  .= '&via='.$via;
+		}
+		return $shareUrl;
 	}
+
+	/**
+	 * Generates GPlus share URL given the url and text, via and hashrags are optional
+	 * @param string $url the url to generate the share link for twetter
+	 * @return string the GPlus share URL. 
+	 */
 
 	public function generateGooglePlusUrl($url){
-
+		$shareUrl  = 'https://plus.google.com/share?url=';
+		$shareUrl .= $url;
+		return $url;
 	}
 
-	public function generateLinkedInUrl($url,$extra=[]){
+	/**
+	 * Generates LinkedIn share URL given the url and extra fields (optional)
+	 * @param string $url the url to generate the share link for twetter
+	 * @param string $extra['title'] the text to tweet (max 150 characters)
+	 * @param string $extra['summary'] the channel name. No '@'
+	 * @param string $extra['source'] the list of comma-separeted hashtags, no '#'
+	 * @return string the LinkedIn share URL
+	 */
 
+	public function generateLinkedInUrl($url,$extra=[]){
+		$shareUrl = 'https://www.linkedin.com/shareArticle?mini=true&url=';
+		$shareUrl .= urlencode($url);
+		if($extra['title']){
+			$shareUrl .=  '&title=';
+			$shareUrl .=  urlencode($extra['title']);
+		}
+		if($extra['summary']){
+			$shareUrl .=  '&summary=';
+			$shareUrl .=  urlencode($extra['summary']);
+		}
+		 if($extra['source']){
+			$shareUrl .=  '&source=';
+			$shareUrl .=  urlencode($extra['source']);
+		}
+		return $shareUrl;
 	}
 }
